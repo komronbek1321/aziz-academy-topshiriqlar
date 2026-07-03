8c87b247041b14cedc211a07a8850e95b9064145
komronbek1321 <b8284661@gmail.com>
2026-07-03 09:20:35 +0500

    Add: Faqat musbatlar yig'indisi [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Faqat_musbatlar_yig_indisi.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+yigindi = 0
+i = 0
+
+while i < n:
+    son = int(input())
+    i += 1
+    
+    if son <= 0:
+        continue
+        
+    yigindi += son
+    
+print(yigindi)
