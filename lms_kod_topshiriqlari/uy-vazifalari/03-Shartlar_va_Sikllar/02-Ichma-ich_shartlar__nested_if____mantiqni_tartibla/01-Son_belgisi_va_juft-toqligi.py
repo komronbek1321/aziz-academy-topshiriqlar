bc1ab4e7ce9e7f294c1bb4c92f9ded220ee6e9e4
komronbek1321 <b8284661@gmail.com>
2026-07-03 08:57:21 +0500

    Add: Son belgisi va juft-toqligi [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Son_belgisi_va_juft-toqligi.py
@@ -0,0 +1,11 @@
+son = int(input())
+
+if son == 0:
+    print("Nol")
+elif son > 0:
+    if son % 2 == 0:
+        print("Musbat juft")
+    else:
+        print("Musbat toq")
+else:
+    print("Manfiy")
