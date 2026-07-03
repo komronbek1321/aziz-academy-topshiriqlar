552cd2a05d6a5a0efd15a07191a2f1c311d74e35
komronbek1321 <b8284661@gmail.com>
2026-07-03 09:23:17 +0500

    Add: Shartli yig'indi [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Shartli_yig_indi.py
@@ -0,0 +1,14 @@
+yigindi = 0
+
+while True:
+    son = int(input())
+    
+    if son == 0 or son >= 100:
+        break
+        
+    if son < 0:
+        continue
+        
+    yigindi += son
+    
+print(yigindi)
