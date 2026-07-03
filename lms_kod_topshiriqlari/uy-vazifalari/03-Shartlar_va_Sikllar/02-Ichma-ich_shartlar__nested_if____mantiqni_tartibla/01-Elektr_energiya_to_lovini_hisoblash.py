73690fc589c79eda3fe03d9e2c52c9d0d3b99f39
komronbek1321 <b8284661@gmail.com>
2026-07-03 09:03:36 +0500

    Add: Elektr energiya to'lovini hisoblash [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Elektr_energiya_to_lovini_hisoblash.py
@@ -0,0 +1,13 @@
+kwh = int(input())
+
+if kwh < 0:
+    print("Notogri qiymat")
+else:
+    if kwh <= 100:
+        summa = kwh * 450
+    elif kwh <= 200:
+        summa = (100 * 450) + ((kwh - 100) * 600)
+    else:
+        summa = (100 * 450) + (100 * 600) + ((kwh - 200) * 900)
+        
+    print(summa)
