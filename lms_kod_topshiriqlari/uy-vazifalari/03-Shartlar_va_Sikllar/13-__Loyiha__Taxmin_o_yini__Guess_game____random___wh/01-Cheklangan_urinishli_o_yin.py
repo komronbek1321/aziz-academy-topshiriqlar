da0609338bf26ee1e9ef7d7882e49257d3e3535d
komronbek1321 <b8284661@gmail.com>
2026-07-03 16:03:37 +0500

    Add: Cheklangan urinishli o'yin [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/01-Cheklangan_urinishli_o_yin.py
@@ -0,0 +1,19 @@
+yashirin = int(input())
+n = int(input())
+
+topdi = False
+
+for _ in range(n):
+    taxmin = int(input())
+    
+    if taxmin == yashirin:
+        print("TOPDINGIZ")
+        topdi = True
+        break
+    elif taxmin > yashirin:
+        print("KATTA")
+    else:
+        print("KICHIK")
+        
+if not topdi:
+    print("YUTQAZDINGIZ")
