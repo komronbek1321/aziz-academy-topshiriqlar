73d16b9ca702c45508e96a6b8df1c82ce32d6fc1
komronbek1321 <b8284661@gmail.com>
2026-07-05 09:17:06 +0500

    Add: Belgi bo'yicha bitta amal [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Belgi_bo_yicha_bitta_amal.py
@@ -0,0 +1,12 @@
+a = int(input())
+op = input().strip()
+b = int(input())
+
+if op == '+':
+    print(a + b)
+elif op == '-':
+    print(a - b)
+elif op == '*':
+    print(a * b)
+else:
+    print("Noma'lum amal")
