1d1d1fae28a96ee2a007c07bd01380d27b6fc7fc
komronbek1321 <b8284661@gmail.com>
2026-07-08 08:09:18 +0500

    Add: Harflar chastotasi (paydo bo'lish tartibida) [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/04-Ma_lumot_Tuzilmalari/07-Dict___asoslar_____key_value__qiymatga_murojaat/01-Harflar_chastotasi__paydo_bo_lish_tartibida_.py
@@ -0,0 +1,11 @@
+soz = input().strip()
+
+chastota = {}
+for harf in soz:
+    if harf in chastota:
+        chastota[harf] += 1
+    else:
+        chastota[harf] = 1
+        
+for harf, soni in chastota.items():
+    print(f"{harf} {soni}")
