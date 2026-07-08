d60f9aa9f3c09d30643ac74fb78634a861190648
komronbek1321 <b8284661@gmail.com>
2026-07-08 08:02:31 +0500

    Add: Poytaxtni topish [komronbek1321]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/04-Ma_lumot_Tuzilmalari/07-Dict___asoslar_____key_value__qiymatga_murojaat/01-Poytaxtni_topish.py
@@ -0,0 +1,9 @@
+poytaxtlar = {
+        "Uzbekistan": "Tashkent",
+    "Russia": "Moscow",
+    "France": "Paris",
+    "Japan": "Tokyo"
+}
+
+mamlakat = input().strip()
+print(poytaxtlar[mamlakat])
